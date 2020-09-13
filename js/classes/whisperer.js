5ef7cd03c42b6b797202ea34f694b68cf4ee593f
Igor Šula <sulaigor@gmail.com>
2020-09-13 14:01:20 +0200

    Clean workspace

--- js/classes/whisperer.js
+++ js/classes/whisperer.js
@@ -1,11 +1,10 @@
 import jQuery from 'jquery';
-import SearchResult from "./search_result";
-import SearchForm from "./search_form";
+import SearchResult from './search_result';
+import SearchForm from './search_form';
 
 let $ = jQuery;
 
 export default class Whisperer {
-
   constructor(searchInputId, searchResultId, otherInputsId) {
     this.searchResult = new SearchResult(searchResultId);
     this.searchForm = new SearchForm(searchInputId, otherInputsId);
@@ -32,52 +31,42 @@ export default class Whisperer {
       this.searchForm.clearOtherInputs();
 
       this.timer = setTimeout(() => {
-        if(this.searchForm.getSearchInputValue().length > 0)
-          this.sendRequest({ street_name: this.searchForm.getSearchInputValue() });
+        if (this.searchForm.getSearchInputValue().length > 0)
+          this.sendRequest({
+            street_name: this.searchForm.getSearchInputValue(),
+          });
       }, time);
     });
   }
 
   sendRequest(params, size = 20) {
-    let requestUrl = `https://${this._userName}:${this._password}@address-database-9251853596.eu-central-1.bonsaisearch.net:443/addresses/_search?default_operator=AND&size=${size}&q=`;
-    let header = btoa(this._userName + ":" + this._password);
+    let requestUrl = `https://eu-central-1.bonsaisearch.net:443/addresses/_search?default_operator=AND&size=${size}&q=`;
+    let header = btoa(this._userName + ':' + this._password);
 
     requestUrl = this.addParamsToRequestUrl(requestUrl, params);
 
-    // console.log(requestUrl);
-    // console.log(header);
-
-    // this.request = fetch(requestUrl, {
-    //   headers: new Headers({
-    //     'Authorization': 'Basic ' + header,
-    //     'Content-Type': 'application/json'
-    //   })
-    // });
-
-    $.ajax({
-      method: 'GET',
-      url: requestUrl,
-      dataType: 'json',
-      beforeSend: function (xhr) {
-        xhr.setRequestHeader("Authorization", "Basic " + header)
-      },
+    this.request = fetch(requestUrl, {
+      headers: new Headers({
+        Authorization: 'Basic ' + header,
+        'Content-Type': 'application/json',
+      }),
     });
+
+    this.request
+      .then((response) => console.log(response))
+      .catch((errors) => console.error(errors));
   }
 
   initAjaxLisener() {
     $(document).ajaxSuccess((event, xhr, settings) => {
       this.response = JSON.parse(xhr.responseText);
       this.response = this.response.hits.hits;
-
-      // console.log(this.response);
-
       this.searchResult.showSearchResult(this.response);
-
     });
   }
 
   addParamsToRequestUrl(url, params) {
-    for(let param in params) url += `${param}:${params[param]}*+`;
+    for (let param in params) url += `${param}:${params[param]}*+`;
     return url;
   }
 }
